d59580ced7a758e26bc6deb352f2f0cd6d144603
shivelle <shivelle@gmx.net>
2017-07-22 10:55:03 +0200

    add basic user authentication

--- src/main.js
+++ src/main.js
@@ -13,19 +13,58 @@ Vue.use(VueRouter)
 import Hello from './components/Hello'
 //import the about component
 import About from './components/About'
+// import the param component
+import Param from './components/Param'
+// import the paramdetails component 
+import paramdetails from './components/paramdetails'
 //define your routes
 const routes = [
-//route for the home route of the web page
+//route for the home route of the webpage
 { path: '/', component: Hello },
-//route for the about route of the web page
-{ path: '/about', component: About }
+//route for the about route of the webpage
+{ path: '/about', component: About }, 
+//route for the param route of the webpage
+{ path: '/param', component: Param }, 
+    //route for the paramdetails passing in params
+    { path: '/Paramdetails/:id', component: paramdetails, name: 'Paramdetails' }
 ]
+
 // Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = new VueRouter({
   routes, // short for routes: routes
   mode: 'history'
+})
+
+//place the router guard
+router.beforeEach((to, from, next) => {
+    //check if the path user is going to is our param path
+    if(to.path == '/param'){
+        //check if the user item is already set
+        if(localStorage.getItem('user')==undefined){
+            //prompt for username
+            var user = prompt('please enter your username');
+            //prompt for password
+            var pass = prompt('please enter your password');
+            //check if th username and password given equals our preset details
+            if (user == 'username' && pass == 'password'){
+            //set the user item
+            localStorage.setItem('user', user);
+            //move to the route
+            next();
+            }else{
+            //alert the username and pass is wrong
+            alert('Wrong username and password, you do not have permission to access that route');
+            //return, do not move to the route
+            return;
+            }
+
+        }
+
+    }
+
+    next()
 })
 //instatinat the vue instance
 new Vue({
